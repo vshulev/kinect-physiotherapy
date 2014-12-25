caa2261b890b420f0324eb949bfff2b9992cf6d2
vshulev <velizar.shulev@gmail.com>
2014-12-25 17:37:54 +0000

    Modify references in patient-desktop-client-engine.

--- patient-desktop-client-engine/BodyDataExtractor.cs
+++ patient-desktop-client-engine/BodyDataExtractor.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WindowsPreview.Kinect;
 
 namespace patient_desktop_client_engine
 {
     class BodyDataExtractor
     {
+
     }
 }
+
+//bool BodyHelper::processJointLocations(
+//            _In_ Windows::Foundation::Collections::IMapView<WindowsPreview::Kinect::JointType, WindowsPreview::Kinect::Joint>^ jointsCollection,
+//            _Out_ Platform::WriteOnlyArray<jointPoint>^ jointPoints)
+//{
+//    if (jointsCollection == nullptr || jointPoints == nullptr)
+//    {
+//        return false;
+//    }
+
+//    ZeroMemory(jointPoints->Data, jointPoints->Length * sizeof(jointPoint));
+
+//    // get active kinect sensor
+//    KinectSensor^ sensor = WindowsPreview::Kinect::KinectSensor::GetDefault();
+
+//    // get coordinatemapper
+//    CoordinateMapper^ coordinateMapper = sensor->CoordinateMapper;
+
+//    for (int jointIndex = 0; jointIndex < (int)jointsCollection->Size; ++jointIndex)
+//    {
+//        // if Z coordinate is negative (joint is behind sensor), set to 0.1f or coordinateMapper will return (-Infinity, -Infinity)
+//        WindowsPreview::Kinect::CameraSpacePoint position = jointsCollection->Lookup((JointType)(jointIndex)).Position;
+//        if (position.Z < 0)
+//        {
+//            position.Z = 0.1f;
+//        }       
+
+//        // map each joint location to depth space
+//        DepthSpacePoint DepthSpacePoint = coordinateMapper->MapCameraPointToDepthSpace(position);
+//        jointPoints->Data[jointIndex].jointType = (JointType)(jointIndex);
+//        jointPoints->Data[jointIndex].x = DepthSpacePoint.X;
+//        jointPoints->Data[jointIndex].y = DepthSpacePoint.Y;
+//    }
+
+//    return true;
+//}
