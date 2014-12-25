433cb2a5b69d13f3c4267a3b65c52f4c575e6b19
vshulev <velizar.shulev@gmail.com>
2014-12-25 17:15:39 +0000

    Add patient-desktop-client-engine to solution.

---
+++ patient-desktop-client-engine/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patient_desktop_client_engine
+{
+    public sealed class Class1
+    {
+    }
+}
