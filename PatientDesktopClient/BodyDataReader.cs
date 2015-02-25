49d5216566f05fffc07bbc68b7bcbd287c171d39
vshulev <velizar.shulev@gmail.com>
2015-02-25 20:13:47 +0000

    Add gesture and gesture db.

---
+++ PatientDesktopClient/BodyDataReader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Kinect;
+
+namespace PatientDesktopClient
+{
+
+    delegate void BodyDataReadHandler(object source, BodyDataReadEventArgs e);
+
+    sealed class BodyDataReader
+    {
+
+        public static readonly BodyDataReader Instance = new BodyDataReader();
+        public event BodyDataReadHandler BodyDataRead;
+        public ulong CurrentTrackingId
+        {
+            get;
+            private set;
+        }
+
+        private Body[] bodies;
+        private IReadOnlyDictionary<JointType, Joint> joints;
+        private IReadOnlyDictionary<JointType, JointOrientation> jointOrientations;
+
+        private BodyDataReader()
+        {
+            KinectSensor sensor = KinectSensor.GetDefault();
+            BodyFrameReader reader = sensor.BodyFrameSource.OpenReader();
+            reader.FrameArrived += frameArrived;
+            bodies = new Body[sensor.BodyFrameSource.BodyCount];
+            sensor.Open();
+        }
+
+        private void frameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            if (BodyDataRead == null)
+                return;
+
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    bodyFrame.GetAndRefreshBodyData(bodies);
+                    storeJointData();
+                    // TODO use BeginInvoke instead
+                    BodyDataRead(this, new BodyDataReadEventArgs(joints, jointOrientations));
+                    //BodyDataRead.BeginInvoke(this, new BodyDataReadEventArgs(joints, jointOrientations), null, null);
+                }
+            }
+            GestureReader.Instance.Track(CurrentTrackingId);
+        }
+
+        private void storeJointData()
+        {
+            CurrentTrackingId = 0;
+            for (int i = 0; i < bodies.Length; i++)
+            {
+                if (bodies[i].IsTracked)
+                {
+                    joints = bodies[i].Joints;
+                    jointOrientations = bodies[i].JointOrientations;
+                    CurrentTrackingId = bodies[i].TrackingId;
+                    break;
+                }
+            }
+        }
+
+    }
+
+    class BodyDataReadEventArgs : EventArgs
+    {
+        public IReadOnlyDictionary<JointType, Joint> Joints;
+        public IReadOnlyDictionary<JointType, JointOrientation> JointOrientations;
+
+        public BodyDataReadEventArgs(IReadOnlyDictionary<JointType, Joint> joints, IReadOnlyDictionary<JointType, JointOrientation> jointOrientations)
+        {
+            Joints = joints;
+            JointOrientations = jointOrientations;
+        }
+    }
+}
---
+++ PatientDesktopClient/GestureScorer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PatientDesktopClient
+{
+
+    delegate void GestureScoreUpdatedHandler(object source, GestureScoreUpdatedEventArgs e);
+
+    class GestureScorer
+    {
+        public static readonly GestureScorer Instance = new GestureScorer();
+        public event GestureScoreUpdatedHandler GestureScoreUpdated;
+        private List<float> results;
+
+        private GestureScorer()
+        {
+            GestureReader.Instance.GestureResultChanged += gestureResultChanged;
+            results = new List<float>();
+
+            //scorers = new List<IScorer>();
+            //scorers.Add(new FluidityScorer());
+        }
+
+        private void gestureResultChanged(object source, GestureResultChangedEventArgs e)
+        {
+            if (GestureScoreUpdated != null)
+            {
+                GestureScoreUpdated(this, new GestureScoreUpdatedEventArgs(0));
+            }
+        }
+    }
+
+    class GestureScoreUpdatedEventArgs : EventArgs
+    {
+
+        public float Score;
+
+        public GestureScoreUpdatedEventArgs(float score)
+        {
+            Score = score;
+        }
+
+    }
+}
